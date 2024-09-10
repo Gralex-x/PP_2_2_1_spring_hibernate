acf465b0acc1e50bd984672edd4242777bdc5584
Gralexx <alexgrigorevkzn@gmail.com>
2024-09-10 14:55:01 +0300

    added join fetch and made fields final for injections

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,6 +4,7 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
@@ -11,29 +12,36 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
+    private final SessionFactory sessionFactory;
+
     @Autowired
-    private SessionFactory sessionFactory;
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
+    @Transactional
     public void add(User user) {
         sessionFactory.getCurrentSession().save(user);
     }
 
     @Override
     @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true)
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("SELECT u FROM User u JOIN FETCH u.car", User.class);
         return query.getResultList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getUsersByCarModelAndSeries(String carModel, int series) {
-        String queryString = "SELECT u FROM User u WHERE u.car.model = :carModel AND u.car.series = :series";
+        String queryString = "SELECT u FROM User u JOIN FETCH u.car c WHERE c.model = :carModel AND c.series = :series";
         return sessionFactory.getCurrentSession()
                 .createQuery(queryString, User.class)
                 .setParameter("carModel", carModel)
-                .setParameter("series", series).getResultList();
+                .setParameter("series", series)
+                .getResultList();
     }
-
-
 }
